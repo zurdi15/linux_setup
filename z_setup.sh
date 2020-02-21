aff5b183126eb699e6cd9b1ff36fc2b7ebd41b3b
Alejandro Atance (TMC) <alejandro.atance@tmceurope.com>
2020-02-21 14:57:45 +0100

    cleaning installation

--- z_setup.sh
+++ z_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/bin/bash
 
 # $1 - Package manager
 
@@ -10,7 +10,7 @@ pm=$1
 
 # Packages
 echo "Installing packages"
-sudo ${pm} update && sudo ${pm} upgrade
+sudo ${pm} update && sudo ${pm} upgrade -y
 sudo ${pm} install curl -y
 sudo ${pm} install vim -y
 sudo ${pm} install zsh -y
@@ -21,8 +21,8 @@ sudo gem install colorls
 
 # Fonts
 echo "Installing fonts"
-if [ ! -d ~/.fonts]; then
-	sudo mkdir ~/.fonts
+if [ ! -d ~/.fonts ]; then
+	mkdir ~/.fonts
 fi
 wget -O ~/.fonts/Ubuntu_Mono_Nerd_Font_Complete.ttf https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete.ttf
 fc-cache -f -v
--- z_setup_zsh.sh
+++ z_setup_zsh.sh
@@ -1,4 +1,4 @@
-#!/bin/env zsh
+#!/bin/zsh
 
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -11,26 +11,31 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/the
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 ## Syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
+echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 ## Fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # Dots
-mkdir ~/tmp
-if[ -f ~/.bash_aliases ];then
+if [ ! -d ~/tmp ]; then
+	mkdir ~/tmp
+fi
+if [ -f ~/.bash_aliases ]; then
 	touch ~/.bash_aliases
 fi
+echo "Creating aliases and dots files"
 echo -e '
-alias l="colorls -lA --sd"\n
-alias ll="colorls -l --tree --sd"\n
-alias lll="colorls -lA --tree --sd"\n
-alias rd="rm -rf"\n
+alias l="colorls -lA --sd"
+alias ll="colorls -l --tree --sd"
+alias lll="colorls -lA --tree --sd"
+alias rd="rm -rf"
 alias shut="shutdown now"' >> ~/.bash_aliases
-DOTS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOTS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/dots
 cp ${DOTS_PATH}/.p10k.zsh ~
 cp ${DOTS_PATH}/.zshrc ~
 cp ${DOTS_PATH}/.vimrc ~
+
+source ~/.zshrc
+clear
