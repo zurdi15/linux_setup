6fd447c64db1f185a563ff37c8a2ad862ddd96ba
Zurdi Zurdo <zurdizurdo25@gmail.com>
2020-11-22 22:11:55 +0100

    lolcat and tilix removed

--- z_setup.sh
+++ z_setup.sh
@@ -24,8 +24,6 @@ sudo ${pm} install vim -y
 sudo ${pm} install zsh -y
 sudo ${pm} install ruby -y
 sudo ${pm} install ruby-dev -y
-sudo ${pm} install lolcat -y
-sudo ${pm} install tilix -y
 sudo ${pm} install build-essential -y
 sudo ${pm} autoremove -y
 sudo gem install colorls
@@ -38,9 +36,6 @@ fi
 wget -O ~/.fonts/Ubuntu_Mono_Nerd_Font_Complete.ttf https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete.ttf
 fc-cache -f -v
 
-# Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # SDKMAN
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
@@ -48,3 +43,7 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 # uLauncher
 wget https://github.com/Ulauncher/Ulauncher/releases/download/5.8.0/ulauncher_5.8.0_all.deb
 sudo dpkg -i ulauncher_5.8.0_all.deb
+
+# Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
