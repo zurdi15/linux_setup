2cb54c2c20b6946c6e12ab4608a596d7cc35ae75
Alejandro Atance <alejandro.atance@atresmedia.com>
2021-03-25 17:46:38 +0100

    feh added

--- install.sh
+++ install.sh
@@ -33,6 +33,7 @@ sudo ${pm} install firefox -y
 sudo ${pm} install rofi -y
 sudo ${pm} install papirus-icon-theme -y
 sudo ${pm} install nautilus -y
+sudo ${pm} install feh -y
 sudo ${pm} install tilix -y
 sudo ${pm} install zsh -y
 sudo ${pm} install ruby -y
