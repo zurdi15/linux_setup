1456e2015efdbc2b55f8f81d892acc90e58ba3bf
Zurdi Zurdo <zurdizurdo25@gmail.com>
2021-03-26 16:42:38 +0100

    manual instructions clarified

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 #rm ulauncher_5.8.0_all.deb
 #git clone https://github.com/KiranWells/ulauncher-nord/ ~/.config/ulauncher/user-themes/nord
 
-echo "When oh my zsh is installed, zsh prompt will appear -> just type exit and the instalation will cotinue :)"
+echo -e "\e[0;49;93mWARNING\e[0;49;0m: When oh my zsh is installed, zsh prompt will appear -> just type exit and the instalation will cotinue"
 read -p "Press any key when you are ready... " key
 
 # Oh-my-zsh
@@ -76,36 +76,6 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 # Ranger devicons
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 
-# Minimal Firefox
-echo """
-		Instructions for minimal Firefox:
-
-    Verify that the user stylesheets (userChrome) option is enabled:
-
-        Go to the address about:config in Firefox
-
-        Search for toolkit.legacyUserProfileCustomizations.stylesheets
-
-        Confirm the option is set to true
-
-    Make sure that you have the Default theme enabled
-        Go to the address about:addons
-        Enable the Default theme if not already enabled
-
-		Locate your Firefox user directory. You should be able to find it by navigating to /home/.mozilla/firefox/ and looking for a directory ending with the world .default-release.
-		Within your Firefox user directory, locate the chrome directory, if one does not already exist you can simply go ahead and create it yourself.
-		
-		Copy the content of dots/neodots/.mozilla/firefox/xxxx.default-release/chrome into your own chrome folder (the one that you just searched/created)
-
-    Select the Customize option from the hamburger menu (≡ ), and remove all items except for:
-
-        Forward button
-        Back button
-        Downloads button
-"""
-
-read -p "Press any key when you are ready... " key
-
 # Copy the .config folder
 echo "Copying dotfiles to ~/.config"
 cp -a dots/neodots/. $HOME
@@ -119,12 +89,64 @@ fi
 fc-cache -f -v
 echo "Installing fonts: Done!"
 
-echo "Installing nvim plugins (and node)"
+echo "Installing nvim plugins"
 nvim -es -u ~/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "qa"
 echo "Installing nvim plugins: done!"
 
-echo "Aditionally, you can use the Nord theme into any intellij-family IDE and set any wallpaper from ~/.config/wallpaper as background"
+# Minimal Firefox
+echo -e """
+\e[0;49;94mInstructions for minimal Firefox:\e[0;49;0m
+	Verify that the user stylesheets (userChrome) option is enabled:
+	 - Go to the address about:config in Firefox
+	 - Search for toolkit.legacyUserProfileCustomizations.stylesheets
+	 - Confirm the option is set to true
 
-echo "Select the nord theme in tilix and disable any kind of border in appearance: Window style -> borderless and Terminal title style -> None."
-echo "Set the Jetbrains Nerd Font 10 on tilix"
-echo "Done!"
+	Make sure that you have the Default theme enabled
+	 - Go to the address about:addons
+	 - Enable the Default theme if not already enabled
+
+	Locate your Firefox user directory. You should be able to find it by navigating to /home/.mozilla/firefox/ and looking for a directory ending with the world .default-release.
+	
+	Within your Firefox user directory, locate the chrome directory, if one does not already exist you can simply go ahead and create it yourself.
+		
+	Copy the content of dots/neodots/.mozilla/firefox/xxxx.default-release/chrome into your own chrome folder (the one that you just searched/created)
+
+	Select the Customize option from the hamburger menu (≡ ), and remove all items except for:
+	 - Forward button
+	 - Back button
+	 - Downloads button
+"""
+read -p "Waiting for Minimal Firefox setup..." key
+
+echo -e """\n
+\e[0;49;94mInstructions for JetBrains IDEs:\e[0;49;0m
+    You can use the Nord theme into any intellij-family IDE and set any wallpaper from ~/.config/wallpaper as background
+"""
+read -p "Waiting for JetBrains IDEs setup..." key
+
+echo -e """\n
+\e[0;49;94mInstructions for Tilix:\e[0;49;0m
+	Select the nord theme in tilix
+	
+	Select JetBrains Nerd Font 10
+	
+	Disable any kind of border in appearance: 
+	 - Window style -> borderless
+	 - Terminal title style -> None
+	
+	Transparency:
+	 - If you are in a Virtual machine, picom will set the transparency on tilix. You can configure it in the ~/.config/picom/picom.conf file
+	 - If you are not in a Virtual Machine, set it in the tilix configuration and remove it from the ~/.config/picom/picom.conf file
+"""
+read -p "Waiting for Tilix setup..." key
+
+echo -e """\n
+\e[0;49;94mInstructions for wlan/eth indicator on polybar:\e[0;49;0m
+	Use the command ip link show to get your own interfaces
+	
+	Change them in the wlan/eth polybar module in ~/.config/polybar/config
+"""
+read -p "Waiting for wlan/eth polybar modules setup..." key
+
+echo -e "\e[0;49;92mDone!\e[0;49;0m"
+
